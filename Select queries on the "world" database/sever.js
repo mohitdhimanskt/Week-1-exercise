ab519562791000875ba9fbe827ab544e672cfb0d
mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-07-10 15:41:12 +0530

    Add to q1

--- Select queries on the "world" database/sever.js
+++ Select queries on the "world" database/sever.js
@@ -10,3 +10,14 @@ var connection = mysql.createConnection({
 connection.connect();
 
 
+//Q1 - greater than 8mil
+
+let q1 = "select name, population from country where population > 8000000 order by population "
+
+connection.query(q1, function (error, results, fields){
+if(error){
+  throw error;
+}
+console.log("Q1 answer", results );
+});
+
